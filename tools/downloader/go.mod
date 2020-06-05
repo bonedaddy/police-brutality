6472d24261145d5a9efe16d056187540a813e17c
postables <postables@rtradetechnologies.com>
2020-06-04 21:36:32 -0700

    tools/downloader: cleans up the cli flags

--- tools/downloader/go.mod
+++ tools/downloader/go.mod
@@ -4,10 +4,12 @@ go 1.14
 
 require (
 	github.com/RTradeLtd/go-ipfs-api/v3 v3.0.0
+	github.com/go-chi/chi v4.1.2+incompatible
 	github.com/panjf2000/ants/v2 v2.4.1
 	github.com/pkg/errors v0.8.1
 	github.com/urfave/cli/v2 v2.2.0
 	go.bobheadxi.dev/zapx/zapx v0.6.8
 	go.uber.org/atomic v1.6.0
+	go.uber.org/multierr v1.5.0
 	go.uber.org/zap v1.15.0
 )
--- tools/downloader/main.go
+++ tools/downloader/main.go
@@ -15,6 +15,10 @@ func main() {
 	app.Usage = "downloads videos listed in the csv file"
 	app.Commands = cli.Commands{
 		&cli.Command{
+			Name:  "webhook-server",
+			Usage: "runs the webhook server",
+		},
+		&cli.Command{
 			Name:  "start",
 			Usage: "starts the downloader",
 			Action: func(c *cli.Context) error {
@@ -35,56 +39,56 @@ func main() {
 				}
 				return nil
 			},
-			Flags: []cli.Flag{
-				&cli.StringFlag{
-					Name:    "directory",
-					Aliases: []string{"dir", "d"},
-					Usage:   "location to save videos to",
-					Value:   "videos",
-				},
-				&cli.StringFlag{
-					Name:    "log.file",
-					Aliases: []string{"lf", "l"},
-					Value:   "downloader.log",
-					Usage:   "where to store log data",
-				},
-				&cli.StringFlag{
-					Name:    "ipfs.endpoint",
-					Aliases: []string{"ie"},
-					Usage:   "endpoint to upload videos to if ipfs uploading is enabled",
-					Value:   "localhost:5001",
-				},
-				&cli.StringFlag{
-					Name:    "ipfs.auth_token",
-					Aliases: []string{"iat"},
-					Usage:   "jwt to use with authentication to an ipfs http api endpoint",
-					EnvVars: []string{"IPFS_AUTH_TOKEN"},
-				},
-				&cli.IntFlag{
-					Name:    "concurrency",
-					Aliases: []string{"con", "c"},
-					Value:   1,
-					Usage:   "enables concurrent download of videos",
-				},
-				&cli.IntFlag{
-					Name:    "max.downloads",
-					Aliases: []string{"md"},
-					Usage:   "maximum number of of incidents to download, 0 indicates all of them",
-					Value:   0,
-				},
-				&cli.DurationFlag{
-					Name:    "timeout",
-					Aliases: []string{"t"},
-					Value:   time.Minute * 3,
-					Usage:   "timeout to quit a download, you may need to adjust depending on your connection",
-				},
-				&cli.BoolFlag{
-					Name:    "upload.to_ipfs",
-					Aliases: []string{"uti"},
-					Usage:   "enables uploading the video data to any ipfs endpoint",
-					Value:   false,
-				},
-			},
+		},
+	}
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:    "directory",
+			Aliases: []string{"dir", "d"},
+			Usage:   "location to save videos to",
+			Value:   "videos",
+		},
+		&cli.StringFlag{
+			Name:    "log.file",
+			Aliases: []string{"lf", "l"},
+			Value:   "downloader.log",
+			Usage:   "where to store log data",
+		},
+		&cli.StringFlag{
+			Name:    "ipfs.endpoint",
+			Aliases: []string{"ie"},
+			Usage:   "endpoint to upload videos to if ipfs uploading is enabled",
+			Value:   "localhost:5001",
+		},
+		&cli.StringFlag{
+			Name:    "ipfs.auth_token",
+			Aliases: []string{"iat"},
+			Usage:   "jwt to use with authentication to an ipfs http api endpoint",
+			EnvVars: []string{"IPFS_AUTH_TOKEN"},
+		},
+		&cli.IntFlag{
+			Name:    "concurrency",
+			Aliases: []string{"con", "c"},
+			Value:   1,
+			Usage:   "enables concurrent download of videos",
+		},
+		&cli.IntFlag{
+			Name:    "max.downloads",
+			Aliases: []string{"md"},
+			Usage:   "maximum number of of incidents to download, 0 indicates all of them",
+			Value:   0,
+		},
+		&cli.DurationFlag{
+			Name:    "timeout",
+			Aliases: []string{"t"},
+			Value:   time.Minute * 3,
+			Usage:   "timeout to quit a download, you may need to adjust depending on your connection",
+		},
+		&cli.BoolFlag{
+			Name:    "upload.to_ipfs",
+			Aliases: []string{"uti"},
+			Usage:   "enables uploading the video data to any ipfs endpoint",
+			Value:   false,
 		},
 	}
 	if err := app.Run(os.Args); err != nil {
